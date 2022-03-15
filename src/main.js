bbea2c2604b902cd460dcc399700bf7a75efc732
Ilya Taratukhin <darkilfa@gmail.com>
2022-03-15 18:18:10 +0300

    add google tag manager

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: process.env.SVELTE_APP_NAME || 'world'
+		name: process.env.SVELTE_APP_NAME || 'world',
+		gtmId: process.env.SVELTE_APP_GTM_ID
 	}
 });
 
